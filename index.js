4aa8c51485b312a4fc8107026181faae60e9ea31
Michael <meguruze@handymaninteractive.com>
2024-01-25 17:54:41 +0000

    Initial commit with 2 Lambda functions.

---
+++ index.js
@@ -0,0 +1,33 @@
+import middy from '@middy/core';
+import httpErrorHandler from '@middy/http-error-handler';
+
+import pg from 'pg';
+
+const { Pool } = pg;
+
+const getDftTraffic = async (event) => {
+
+    const pool = new Pool({
+        host: process.env.HOSTNAME,
+        user: process.env.USERNAME,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE,
+        port: '5432',
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
+
+    const client = await pool.connect();
+    const { rows } = await client.query('SELECT * FROM dfttraffictable LIMIT');
+    client.release();
+    return {
+        statusCode: 200,
+        body: JSON.stringify(rows)
+    };
+};
+
+export const handler = middy()
+  .use(httpErrorHandler())
+  .handler(getDftTraffic);
+
